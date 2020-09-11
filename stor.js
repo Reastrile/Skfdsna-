4e7964027ae48f0bd500315c9c09172aba472d98
Reastrile <71111963+Reastrile@users.noreply.github.com>
2020-09-11 17:11:17 -0500

    Update stor.js

--- stor.js
+++ stor.js
@@ -1,2 +1,3 @@
-export var password = "b`O54&axnRxX{nF34XOd},OP,*QG[W4lftYBHLRHsZb9VbU44RB[#aX@*x:tIV/NW%m#G:0AnMD2NXgnI}k!*ZM6;a6h@nQTG{7k[%+DIEhb!jJavE[g"
-export var encrypteddisplaytext = "BDCEHCCCCBFCCICCFCFD";
+var password = "b`O54&axnRxX{nF34XOd},OP,*QG[W4lftYBHLRHsZb9VbU44RB[#aX@*x:tIV/NW%m#G:0AnMD2NXgnI}k!*ZM6;a6h@nQTG{7k[%+DIEhb!jJavE[g"
+var encrypteddisplaytext = "BDCEHCCCCBFCCICCFCFD";
+export { password, encrypteddisplaytext };
